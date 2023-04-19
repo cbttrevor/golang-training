cfd88a7202c20d91fccb98e074b1f328b2e1d2d5
Trevor Sullivan <trevor@trevorsullivan.net>
2023-04-19 12:20:37 -0600

    🧠 Initial commit for Go structs

---
+++ 06-go-structs/structs.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Car struct {
+	manufacturer string
+	model        string
+	moonroof     bool
+	horsepower   uint16
+	year         uint16
+	owner        *Person
+}
+
+type Person struct {
+	firstName string
+	lastName  string
+	company   string
+	age       uint8
+}
+
+func main() {
+	person01 := Person{firstName: "Trevor", lastName: "Sullivan", company: "CBT Nuggets", age: 25}
+	person02 := Person{firstName: "John", lastName: "McGovern", company: "CBT Nuggets", age: 27}
+
+	person01.firstName = "John"
+	person01.age = 32
+	fmt.Printf("%v %v is my name, and I work for %v\n", person01.firstName, person01.lastName, person01.company)
+	fmt.Printf("%v's age is %v\n", person01.firstName, person01.age)
+
+	fmt.Printf("%+v\n", person01)
+	fmt.Printf("%p\n", &person01)
+	fmt.Printf("%p\n", &person02)
+
+	testStructRefs()
+}
+
+func testStructRefs() {
+	car01 := Car{manufacturer: "Hyundai", model: "Santa Fe", year: 2015, moonroof: true, horsepower: 220}
+	carOwner := Person{firstName: "Daniel", lastName: "Williams", age: 42, company: "CBT Nuggets"}
+	car01.owner = &carOwner
+
+	carOwner.firstName = "Trevor"
+
+	fmt.Printf("%+v\n", car01.owner)
+	fmt.Printf("%p %p\n", &car01.owner, &carOwner)
+}
